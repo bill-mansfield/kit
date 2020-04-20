b9a6c0f159727eb126976b0a09bc317aed5a1e08
bill <manbil03@gmail.com>
2020-04-20 12:11:04 +0930

    app:services:use useeffect hook to call firebase db

--- src/components/Dashboard/stats/metres-climbed.js
+++ src/components/Dashboard/stats/metres-climbed.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import firebase from '../../../services/firebase';
 import { Typography } from '@material-ui/core';
@@ -11,36 +11,57 @@ const useStyles = makeStyles((theme) => ({
 
 export default function MetresClimbed() {
     const classes = useStyles();
+    const [data, setData] = useState({ ascents: [] });
 
-    async function ascents() {
-        return await firebase.getCurrentUserAscents();
-    }
 
-    const metres = (value) => {
+    // useEffect(() => {
+    //     async function fetchData() {
+    //       // You can await here
+    //       const response = await MyAPI.getData(someId);
+    //       // ...
+    //     }
+    //     fetchData();
+    //   }, [someId]); // Or [] if effect doesn't need props or state
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+          const result = await firebase.getCurrentUserAscents();
+          setData(result);
+        };
+        fetchData();
+      }, []);
+
+    console.log(data);
+
+
+    const getMetresClimbed = () => {
         let totalMetres = 0;
 
-        for (let i = 0; i < value.length - 1; i++) {
-            let climbLength = value[i].file[7];
+        for (let i = 0; i < data.length - 1; i++) {
+            let climbLength = data[i].file[7];
 
             climbLength = climbLength.replace('m', '');
             climbLength = parseInt(climbLength);
             totalMetres += climbLength;
         }
         return totalMetres;
-    }
-
-    const renderMetresClimbed = () => {
-        ascents().then(function(value) {
-            let height = metres(value) + ' metres climbed';
-            return height;
-        })
     };
 
-    renderMetresClimbed();
 
     return (
-        <Typography className={classes.metresClimbed} variant="h3">
-            Metres climbed:
-        </Typography>
+        <>
+            <Typography className={classes.metresClimbed} variant="h3">
+                Metres climbed: 
+            </Typography>
+            {/* <ul> */}
+            {/* {data.ascents.map(item => (
+                 <li>
+                     {console.log(item)}
+                     {item[1].file[7]}
+                 </li>
+                ))}
+            </ul> */}
+        </>
     )
 }
